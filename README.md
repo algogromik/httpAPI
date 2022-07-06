d06e5f54630db818b9b37b5134ee040cb0d1808e
NoamSfez <noam.sfez@gmail.com>
2022-07-06 21:29:06 +0300

    Change to real Firebase Project Add import data files to the emulator

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
 #### Launching
 
-### Kill ports (in case of previous error in the emulator) and then run emulator
+### Kill ports (in case of previous error in the emulator) and then simple run emulator
+
+If the emulator has stopped due to an error you have to kill all used ports
 
 ```bash
 $ kill-port --port 8000,9099,5001,8080,9000,5000,8085,9199,4000,4400,4500 && firebase emulators:start
 ```
+
+### Backup import/export data in emulator
+
+## Import data in emulator
+
+Add production data in the backup file at:
+
+.../httpAPI/emulators.backup/database_export/functions-api-162ea.json
+
+Then start with importing it:
+
+```bash
+$ kill-port --port 8000,9099,5001,8080,9000,5000,8085,9199,4000,4400,4500 && firebase emulators:start --import=./emulators.backup
+```
+
+## Export data from emulator
+
+If you want to updates the data with the last changes from the emulator, you have to open a new terminal and run the next command before stoping it:
+
+```bash
+$ firebase emulators:export ./emulators.backup
+```
+
+Then, you will be able to overwrite past backup
 
 ### Tunneling Network
 
@@ -18,7 +44,7 @@ $ ngrok http 5001
 
 then you will receive a key (like 77ea-2a0d-6fc2-48c0-4300-3d82-8127-670a-b48f) and you can consume the api on:
 
-https://77ea-2a0d-6fc2-48c0-4300-3d82-8127-670a-b48f.ngrok.io/functions-learning-77e34/us-central1/api_express/{function_name}
+https://77ea-2a0d-6fc2-48c0-4300-3d82-8127-670a-b48f.ngrok.io/functions-api-162ea/us-central1/api_express/{function_name}
 
 ## with LocalTunnel (currently not available)
 
@@ -37,20 +63,16 @@ $ lt --port 5001 --subdomain cloud-functions
 ```
 
 base-url: https://cloud-functions.loca.lt
-project's pathname: /functions-learning-77e34/us-central1/{function_name}
+project's pathname: /functions-api-162ea/us-central1/{function_name}
 
-example: https://cloud-functions.loca.lt/functions-learning-77e34/us-central1/api_express
+example: https://cloud-functions.loca.lt/functions-api-162ea/us-central1/api_express
 
 #### Testing scripts:
 
-## Init database with few data
-
-{base_URL}/functions-learning-77e34/us-central1/api_express/init
-
 ## Get odds for a sport
 
-{base_URL}/functions-learning-77e34/us-central1/api_express/?sport=soccer
+{base_URL}/functions-api-162ea/us-central1/api_express/?sport=soccer
 
 ## Get an event's odds
 
-{base_URL}/functions-learning-77e34/us-central1/api_express/?sport=soccer&eventID=1000
+{base_URL}/functions-api-162ea/us-central1/api_express/?sport=soccer&eventID=1000
--- functions/index.js
+++ functions/index.js
@@ -10,66 +10,66 @@ var database = admin.database();
 const app = express();
 app.use(cors({ origin: true }));
 
-const initRTDB = async () => {
-  var updates = {};
-  updates["/Soccer/"] = {
-    "Premier League": {
-      "Manchester vs Chelsea": {
-        ID: 1000,
-      },
-    },
-    "Second League": {
-      "Barcelone vs Real Madrid": {
-        ID: 2000,
-      },
-      "PSG vs Marseille": {
-        ID: 3000,
-      },
-    },
-  };
-  updates["/Basket/"] = {
-    "League 1": {
-      "TeamA vs TeamB": {
-        ID: 4000,
-      },
-    },
-    "League 2": {
-      "TeamC vs TeamD": {
-        ID: 5000,
-      },
-      "TeamE vs TeamF": {
-        ID: 6000,
-      },
-    },
-  };
-  updates["/Odds/"] = {
-    1000: {
-      odds1: 3.4,
-      odds2: 4.5,
-    },
-    2000: {
-      odds1: 1.1,
-      odds2: 2.7,
-    },
-    3000: {
-      odds1: 3.6,
-      odds2: 4.1,
-    },
-    4000: {
-      odds1: 2.4,
-      odds2: 4.5,
-    },
-    5000: {
-      odds1: 2.1,
-      odds2: 0.7,
-    },
-    6000: {
-      odds1: 1.6,
-      odds2: 3.9,
-    },
-  };
-  await database.ref().update(updates);
-};
+// const initRTDB = async () => {
+//   var updates = {};
+//   updates["/Soccer/"] = {
+//     "Premier League": {
+//       "Manchester vs Chelsea": {
+//         ID: 1000,
+//       },
+//     },
+//     "Second League": {
+//       "Barcelone vs Real Madrid": {
+//         ID: 2000,
+//       },
+//       "PSG vs Marseille": {
+//         ID: 3000,
+//       },
+//     },
+//   };
+//   updates["/Basket/"] = {
+//     "League 1": {
+//       "TeamA vs TeamB": {
+//         ID: 4000,
+//       },
+//     },
+//     "League 2": {
+//       "TeamC vs TeamD": {
+//         ID: 5000,
+//       },
+//       "TeamE vs TeamF": {
+//         ID: 6000,
+//       },
+//     },
+//   };
+//   updates["/Odds/"] = {
+//     1000: {
+//       odds1: 3.4,
+//       odds2: 4.5,
+//     },
+//     2000: {
+//       odds1: 1.1,
+//       odds2: 2.7,
+//     },
+//     3000: {
+//       odds1: 3.6,
+//       odds2: 4.1,
+//     },
+//     4000: {
+//       odds1: 2.4,
+//       odds2: 4.5,
+//     },
+//     5000: {
+//       odds1: 2.1,
+//       odds2: 0.7,
+//     },
+//     6000: {
+//       odds1: 1.6,
+//       odds2: 3.9,
+//     },
+//   };
+//   await database.ref().update(updates);
+// };
 
 app.get("/", async (req, res) => {
   const eventID = req.query.eventID;
@@ -123,6 +123,6 @@ app.get("/", async (req, res) => {
   res.status(200).send(data);
 });
 
-initRTDB();
+// initRTDB();
 
 exports.api_express = functions.https.onRequest(app);
